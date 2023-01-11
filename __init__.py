3b05765d7c5985a73e71ebac5923b14f449b5e86
jansauven <janteiklu@gmail.com>
2023-01-12 00:59:12 +0100

    changes

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-
+from datasets import make_irm_friedman
+from datasets import make_plr_fingerhut2018
+from datasets import make_irm_farell2021
+from doubleml_skorch_api import NeuralNetRegressorDoubleML
+from doubleml_skorch_api import NeuralNetRegressorXdoubleML
+from simulation_base_class import simulation_study
