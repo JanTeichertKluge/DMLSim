e1214367d5b511e68b7cf7935db582889ae48c8a
Jan Teichert-Kluge <68404051+JanTeichertKluge@users.noreply.github.com>
2023-01-12 10:55:23 +0100

    Update __init__.py

--- dml_sim/__init__.py
+++ dml_sim/__init__.py
@@ -1,1 +1,15 @@
+from .datasets import make_irm_friedman
+from .datasets import make_plr_fingerhut2018
+from .datasets import make_irm_farell2021
 
+from .doubleml_skorch_api import NeuralNetRegressorXdoubleML
+from .doubleml_skorch_api import NeuralNetRegressorDoubleML
+
+from .simulation_base_class import simulation_study
+
+__all__ = ['make_irm_friedman',
+           'make_plr_fingerhut2018',
+           'make_irm_farell2021',
+           'NeuralNetRegressorXdoubleML',
+           'NeuralNetRegressorDoubleML',
+           'simulation_study']
