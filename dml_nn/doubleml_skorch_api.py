9f7f252d941c28f2d8ea226b7b8c8a0fc81e1ed0
Jan Teichert-Kluge <68404051+JanTeichertKluge@users.noreply.github.com>
2023-01-13 11:31:18 +0100

    Update doubleml_skorch_api.py

--- dml_nn/doubleml_skorch_api.py
+++ dml_nn/doubleml_skorch_api.py
@@ -45,5 +45,31 @@ class NeuralNetRegressorDoubleML:
         return self
 
     def predict(self, X):
-        preds = self.torch_nn.predict(X)
+        preds = self.torch_nn.predict_proba(X)
+        return preds.reshape(-1)
+    
+    
+    
+class NeuralNetRegressorDoubleML_prob:
+    _estimator_type = 'regressor'
+
+    def __init__(self, *args, **kwargs):
+        self.torch_nn = NeuralNetRegressor(*args, **kwargs)
+
+
+    def set_params(self, **params):
+        self.torch_nn.set_params(**params)
+        return self
+
+    def get_params(self, deep=True):
+        dict = self.torch_nn.get_params(deep)
+        return dict
+
+    def fit(self, X, y):
+        y = y.reshape(-1,1)
+        self.torch_nn.fit(X, y)
+        return self
+
+    def predict(self, X):
+        preds = self.torch_nn.predict_proba(X)
         return preds.reshape(-1)
