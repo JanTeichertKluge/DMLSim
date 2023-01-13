4c84ad57cccfb925a6dacbbb0eddf5572e14cb15
Moritz-Sundermann <119783237+Moritz-Sundermann@users.noreply.github.com>
2023-01-13 23:52:19 +0100

    Update datasets.py from dev
    Add reference and docstrings
    
    * Update datasets.py
    
    Adding references
    
    * Update datasets.py
    
    preparing Args in docstrings
    
    updated docstings, removed alpha as a parameter for IRM DGPs
    
    removed Fingerhut docstring to test
    
    * Update datasets.py
    
    added Fingerhut docstrings again
    
    * Update datasets.py
    
    Removed Fingerhut DGP with comments to test

--- dml_sim/datasets.py
+++ dml_sim/datasets.py
@@ -6,28 +6,55 @@ from sklearn.datasets import make_spd_matrix
 from doubleml import DoubleMLData, DoubleMLClusterData
 from scipy.special import expit
 
+#for make_plr_fingerhut2018
+from functools import reduce
+from operator import mul
+
 _array_alias = ['array', 'np.ndarray', 'np.array', np.ndarray]
 _data_frame_alias = ['DataFrame', 'pd.DataFrame', pd.DataFrame]
 _dml_data_alias = ['DoubleMLData', DoubleMLData]
 
-def make_irm_friedman(n_obs=500, dim_x=20, alpha=None, return_type='DoubleMLData', **kwargs):
-    """
-    Args:
-    
-    Generating data from a five dimensional test function
-    which was proposed to assess predictive performance of
-    methods and then extended to evaluate causal inference
+def make_irm_friedman(n_obs=500, dim_x=20, return_type='DoubleMLData', **kwargs):
+    """  
+    Generates data from a five dimensional test function which was proposed to assess 
+    predictive performance of methods and then extended to evaluate causal inference 
     methods for some highly non-linear outcome (Parikh et al. 2022)
     
-    References: Friedman, Jerome H., Eric Grosse, and Werner Stuetzle (June 1983). “Multidimensional
-    Additive Spline Approximation”. In: SIAM Journal on Scientific and Statistical
-    Computing 4.2, pp. 291–301. doi: 10.1137/0904023. 
+    References:
+        - Friedman, Jerome H., Eric Grosse, and Werner Stuetzle (June 1983). “Multidimensional
+          Additive Spline Approximation”. In: SIAM Journal on Scientific and Statistical
+          Computing 4.2, pp. 291–301. doi: 10.1137/0904023. 
+        - Friedman, Jerome H. (1991). “Multivariate Adaptive Regression Splines”. In: The
+          Annals of Statistics 19.1. Publisher: Institute of Mathematical Statistics, pp. 1–67. doi:
+          10.1214/aos/1176347963.
+        - Parikh, Harsh et al. (June 2022). “Validating Causal Inference Methods”. In: Proceedings
+          of the 39th International Conference on Machine Learning. ISSN: 2640-3498. PMLR,
+          pp. 17346–17358. url: https://proceedings.mlr.press/v162/parikh22a.html
     
-    
-    Friedman 1991; Parikh et al. 2022; Chipman, George, and McCulloch 2010.
-    
-    
-    
+    Arguments
+    ----------
+    n_obs : int
+        The number of observations to generate.
+    dim_x : int
+        The number of features in the input data.
+    return_type : str
+        The format in which to return the data. Can be one of 'DoubleMLData', 'dataframe', or 'array'.
+    **kwargs : dict
+        Additional keyword arguments to pass to the function.
+        
+    Returns
+    -------
+    x, y, d, theta : numpy array
+        The generated input data, output variable, treatment variable, and variable for the heterogeneous treatment effect, respectively.
+        Returned if return_type='array'.
+    (data, theta) : tuple
+        A tuple containing pandas DataFrame of the generated data with columns for input variables, output variable, and treatment variable
+        and variable for the heterogeneous treatment effect.
+        Returned if return_type='dataframe'.
+    (data, theta) : tuple
+        A tuple containing DoubleMLData object containing the generated data with columns for input variables,output variable, treatment variable
+        and variable for the heterogeneous treatment effect.
+        Returned if return_type='DoubleMLData'.
     """
     x = np.random.uniform(0, 1, (n_obs, dim_x))
     px = expit(x[:, 0] + x[:, 1] - 0.5)
@@ -44,31 +71,54 @@ def make_irm_friedman(n_obs=500, dim_x=20, alpha=None, return_type='DoubleMLData
         return x, y, d, theta
     elif return_type in _data_frame_alias + _dml_data_alias:
         x_cols = [f'X{i + 1}' for i in np.arange(dim_x)]
-        data = pd.DataFrame(np.column_stack((x, y, d)),
+        data = pd.DataFrame(np.column_stack((x, y, d)), 
                             columns=x_cols + ['y', 'd'])
         if return_type in _data_frame_alias:
-            return data
+            return (data, theta)
         else:
-            return DoubleMLData(data, 'y', 'd', x_cols)
+            return (DoubleMLData(data, 'y', 'd', x_cols), theta)
     else:
         raise ValueError('Invalid return_type.')
 
-
-
-from functools import reduce
-from operator import mul
-
+        
 def make_plr_fingerhut2018(n_obs=500, dim_x=20, theta=1, return_type='DoubleMLData', **kwargs):
-    """
+    """ 
+    Generates data from a PLR model as used in Fingerhut, Sesia, and Romano 2022.
     
-    Args:
+    References: 
+        - Fingerhut, Nitai, Matteo Sesia, and Yaniv Romano (June 2022). "Coordinated Dou-
+          ble Machine Learning".  doi: 10.48550/arXiv.2206.00885. 
+        - Code for data generation available online. Link to GitHub Repository: 
+          https://github.com/nitaifingerhut/C-DML/tree/9855dd8c7f6fcff0b082822f0d9dd355573715a7
     
-    Generating data from a PLR model as used in Fingerhut, Sesia, and Romano 2022.
-    
-    References: Fingerhut, Nitai, Matteo Sesia, and Yaniv Romano (June 2022). "Coordinated Dou-
-    ble Machine Learning".  doi: 10.48550/arXiv.2206.00885. 
-    
-    
+    Parameters
+    ----------
+    n_obs : int
+        The number of observations to generate.
+    dim_x : int
+        The number of features in the input data.
+    theta : float
+        The true value of the treatment effect. If None, a default value will be used.
+    return_type : str
+        The format in which to return the data. Can be one of 'DoubleMLData', 'dataframe', or 'array'.
+    **kwargs : dict
+        Additional keyword arguments to pass to the function.
+        - rho : float
+            correlation parameter
+        - majority_s : float
+            threshold to divide samples into two disjoint groups 
+        
+    Returns
+    -------
+    x, y, d : numpy array
+        The generated input data, output variable, and treatment variable, respectively.
+        Returned if return_type='array'.
+    data : pandas DataFrame
+        The generated data with columns for input variables, output variable, and treatment variable.
+        Returned if return_type='dataframe'.
+    data : DoubleMLData
+        A wrapper object containing the generated data with columns for input variables, output variable, and treatment variable.
+        Returned if return_type='DoubleMLData'.
     """
     rrho = kwargs.get('rho', 0.8)
     majority_s = kwargs.get('majority_s', 0.75)
@@ -113,18 +163,41 @@ def make_plr_fingerhut2018(n_obs=500, dim_x=20, theta=1, return_type='DoubleMLDa
 
 
 
-
-def make_irm_farell2021(n_obs=500, dim_x=20, alpha=None, return_type='DoubleMLData', **kwargs):
-    """
-    Args:
+def make_irm_farell2021(n_obs=500, dim_x=20, return_type='DoubleMLData', **kwargs):
     
-    Generating data based on a non-linear model for treatment assignment dependent on the covariates as used
+    """
+    Generates data based on a non-linear model for treatment assignment dependent on the covariates as used
     in Farrell, Liang, and Misra 2021.
    
-    References: Farrell, Max H., Tengyuan Liang, and Sanjog Misra (2021). “Deep Neural Networks for Esti-
-    mation and Inference”. en. In: Econometrica 89.1. doi: 10.3982/ECTA16901. 
-  
-   
+    References:
+        - Farrell, Max H., Tengyuan Liang, and Sanjog Misra (2021). “Deep Neural Networks for Esti-
+          mation and Inference”. en. In: Econometrica 89.1. doi: 10.3982/ECTA16901. 
+        - Code for data generation available online.
+
+    Parameters
+    ----------
+    n_obs : int
+        The number of observations to generate.
+    dim_x : int
+        The number of features in the input data.
+    return_type : str
+        The format in which to return the data. Can be one of 'DoubleMLData', 'dataframe', or 'array'.
+    **kwargs : dict
+        Additional keyword arguments to pass to the function.
+        
+    Returns
+    -------
+    x, y, d, theta : numpy array
+        The generated input data, output variable, treatment variable, and variable for the heterogeneous treatment effect, respectively.
+        Returned if return_type='array'.
+    (data, theta) : tuple
+        A tuple containing pandas DataFrame of the generated data with columns for input variables, output variable, and treatment variable
+        and variable for the heterogeneous treatment effect.
+        Returned if return_type='dataframe'.
+    (data, theta) : tuple
+        A tuple containing DoubleMLData object containing the generated data with columns for input variables,output variable, treatment variable
+        and variable for the heterogeneous treatment effect.
+        Returned if return_type='DoubleMLData'.
     """
     x = np.random.uniform(0, 1, (n_obs, dim_x))
     px = expit(x[:, 0] + x[:, 1] - 0.5)
@@ -141,11 +214,11 @@ def make_irm_farell2021(n_obs=500, dim_x=20, alpha=None, return_type='DoubleMLDa
         return x, y, d, theta
     elif return_type in _data_frame_alias + _dml_data_alias:
         x_cols = [f'X{i + 1}' for i in np.arange(dim_x)]
-        data = pd.DataFrame(np.column_stack((x, y, d)),
+        data = pd.DataFrame(np.column_stack((x, y, d)),   
                             columns=x_cols + ['y', 'd'])
         if return_type in _data_frame_alias:
-            return data
+            return (data, theta)
         else:
-            return DoubleMLData(data, 'y', 'd', x_cols)
+            return (DoubleMLData(data, 'y', 'd', x_cols), theta) 
     else:
         raise ValueError('Invalid return_type.')
